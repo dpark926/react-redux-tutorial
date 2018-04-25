74bc5eda229f0f315cd37c0c8a8cb4ef4aa004c4
Daniel Park <dpeace926@gmail.com>
2018-04-25 19:01:53 -0400

    create the store, need to pass in reducer

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+
+function reducer() {
+  return 'State';
+}
+
+const store = createStore( reducer );
+
+console.log( store.getState() );
+
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
