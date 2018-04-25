7a19f98f535a4ac92c11ca1e781bad6e9021a955
Daniel Park <dpeace926@gmail.com>
2018-04-25 19:45:10 -0400

    more real-world store/reducer, store is obj

--- src/index.js
+++ src/index.js
@@ -4,47 +4,67 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore } from 'redux';
+// combineReducers allows multiple reducers to be combined into a single reducer
+// to be passed into the store
+import { combineReducers, createStore } from 'redux';
 
-// REDUCER
-// how does a reducer read an action to update the store's state?
-// it had 2 parameters: the initial state of the reducer, an action
-// a reducer listens to every action that is sent
-// need to figure what to do for each action
-function reducer( state, action ) {
-  // make sure the the reducer is receiving the action
-  console.log( action );
-
-  if ( action.type === 'changeState' ) {
-    return action.payload.newState;
-  }
-
-  return 'State';
+// REDUCERS
+function productsReducer( state = [], action ) {
+  return state;
 };
+
+function userReducer( state = '', action ) {
+  return state;
+};
+
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+})
 
 // STORE
-// createStore must pass a reducer
-const store = createStore( reducer );
-
-// see the state of the store
-// before action dispatched
+const store = createStore( allReducers );
 console.log( store.getState() );
 
-// ACTION
-// object with a type and payload
-const action = {
-  type: 'changeState',
-  payload: {
-    newState: 'New State'
-  }
-};
-
-// DISPATCH
-// send action to store
-store.dispatch( action );
-
-// logging state after action dispatched
-console.log( store.getState() );
+// // REDUCER
+// // how does a reducer read an action to update the store's state?
+// // it had 2 parameters: the initial state of the reducer, an action
+// // a reducer listens to every action that is sent
+// // need to figure what to do for each action
+// function reducer( state, action ) {
+//   // make sure the the reducer is receiving the action
+//   console.log( action );
+//
+//   if ( action.type === 'changeState' ) {
+//     return action.payload.newState;
+//   }
+//
+//   return 'State';
+// };
+//
+// // STORE
+// // createStore must pass a reducer
+// const store = createStore( reducer );
+//
+// // see the state of the store
+// // before action dispatched
+// console.log( store.getState() );
+//
+// // ACTION
+// // object with a type and payload
+// const action = {
+//   type: 'changeState',
+//   payload: {
+//     newState: 'New State'
+//   }
+// };
+//
+// // DISPATCH
+// // send action to store
+// store.dispatch( action );
+//
+// // logging state after action dispatched
+// console.log( store.getState() );
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
