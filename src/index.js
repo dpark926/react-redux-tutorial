58ff0f63fc10173fa0f267a44ebb7c85880d2272
Daniel Park <dpeace926@gmail.com>
2018-04-25 19:15:20 -0400

    create an action and dispatch to store

--- src/index.js
+++ src/index.js
@@ -8,11 +8,27 @@ import { createStore } from 'redux';
 
 function reducer() {
   return 'State';
-}
+};
 
+// STORE
+// createStore must pass a reducer
 const store = createStore( reducer );
 
+// see the state of the store
 console.log( store.getState() );
+
+// ACTION
+// object with a type and payload
+const action = {
+  type: 'changeState',
+  payload: {
+    newState: 'New State'
+  }
+};
+
+// DISPATCH
+// send action to store
+store.dispatch( action );
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
