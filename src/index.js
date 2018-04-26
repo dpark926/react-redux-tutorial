35c02c1dc63434638035af8abf98d933252ce9df
Daniel Park <dpeace926@gmail.com>
2018-04-25 20:31:01 -0400

    redux file structure/organization

--- src/index.js
+++ src/index.js
@@ -11,18 +11,8 @@ import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 // REDUCERS
-function productsReducer( state = [], action ) {
-  return state;
-};
-
-function userReducer( state = '', { type, payload } ) {
-  switch ( type ) {
-    case 'updateUser':
-      return payload;
-  }
-
-  return state;
-};
+import productsReducer from './reducers/products-reducer';
+import userReducer from './reducers/user-reducer';
 
 const allReducers = combineReducers({
   products: productsReducer,
